abdb506073820cc28064d1d961d9f48e45a1027d
Ilya_Chvilyov <life270317@gmail.com>
2024-08-22 17:57:47 +0300

    Added validators.

---
+++ EventsWebApp/Application/Common/Validators/EventParticipantValidator.cs
@@ -0,0 +1,18 @@
+﻿using EventsWebApp.Domain.Models;
+using FluentValidation;
+
+namespace EventsWebApp.Application.Common.Validators
+{
+    public class EventParticipantValidator : AbstractValidator<EventParticipant>
+    {
+        public EventParticipantValidator()
+        {
+            RuleFor(eventParticipant =>
+                eventParticipant.Email).EmailAddress().NotEmpty().MaximumLength(50);
+            RuleFor(eventParticipant =>
+                eventParticipant.Firstname).NotEmpty().MaximumLength(20);
+            RuleFor(eventParticipant =>
+                eventParticipant.Lastname).NotEmpty().MaximumLength(20);
+        }
+    }
+}
---
+++ EventsWebApp/Application/Common/Validators/EventValidator.cs
@@ -0,0 +1,17 @@
+﻿using EventsWebApp.Domain.Models;
+using FluentValidation;
+
+public class EventValidator : AbstractValidator<Event>
+{
+    public EventValidator()
+    {
+        RuleFor(@event =>
+            @event.Title).NotEmpty().MaximumLength(20);
+        RuleFor(@event =>
+            @event.Description).MaximumLength(300);
+        RuleFor(@event =>
+            @event.Category).MaximumLength(20);
+        RuleFor(@event =>
+            @event.MaxParticipants).GreaterThan(0);
+    }
+}
