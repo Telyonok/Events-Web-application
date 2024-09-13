05371d060477de31128ba0f0cac680bb5aad5e75
Ilya_Chvilyov <life270317@gmail.com>
2024-09-13 17:00:57 +0300

    Updated migrations. Picture is now nullable.

--- EventsWebApp/Infrastructure/Migrations/20240913101444_Topup.Designer.cs
+++ EventsWebApp/Infrastructure/Migrations/20240913101444_Topup.Designer.cs
@@ -4,6 +4,7 @@ using EventsWebApp.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EventsWebApp.Migrations
 {
     [DbContext(typeof(EventsDbContext))]
-    partial class EventsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240913101444_Topup")]
+    partial class Topup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
